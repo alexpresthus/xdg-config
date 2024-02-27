4c4514f3557bb1cb37db6eaf296567cee6b64b98
alexpresthus <alexander.presthus@gmail.com>
2024-02-27 22:59:46 +0100

    feat(snippets): remap docstring nav.

--- nvim/lua/alex/lazy/snippets.lua
+++ nvim/lua/alex/lazy/snippets.lua
@@ -18,13 +18,8 @@ return {
             ls.filetype_extend("javascript", { "jsdoc" })
 
             vim.keymap.set("i", "<C-K>", function () ls.expand() end, { silent = true })
-            vim.keymap.set({"i", "s"}, "<leader>,", function () ls.jump(1) end, { silent = true })
-            vim.keymap.set({"i", "s"}, "<leader>;", function () ls.jump(-1) end, { silent = true })
-            vim.keymap.set({"i", "s"}, "<leader>.", function ()
-                if ls.choice_active then
-                    ls.change_choice(1)
-                end
-            end, { silent = true })
+            vim.keymap.set({"i", "s"}, "<C-j>", function () ls.jump(1) end, { silent = true })
+            vim.keymap.set({"i", "s"}, "<C-k>", function () ls.jump(-1) end, { silent = true })
         end
     },
     {
