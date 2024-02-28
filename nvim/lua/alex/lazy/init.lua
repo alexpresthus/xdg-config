f0d585855c2ff99c94dc1ff81d06e719518bed10
alexpresthus <alexander.presthus@gmail.com>
2024-02-28 21:54:06 +0100

    feat(nvim): add plugin vim-apm!

--- nvim/lua/alex/lazy/init.lua
+++ nvim/lua/alex/lazy/init.lua
@@ -12,4 +12,15 @@ return {
         "alexpresthus/julian.nvim",
         opts = {}
     },
+    {
+        "ThePrimeagen/vim-apm",
+        lazy = false,
+        config = function()
+            local apm = require("vim-apm")
+            apm:setup({})
+            vim.keymap.set("n", "<leader>apm", function()
+               apm:toggle_monitor()
+            end)
+        end
+    },
 }
