1a8bb26cf55f180a539f46c0c4dd7cbc4efcf159
alexpresthus <alexander.presthus@gmail.com>
2024-03-04 23:38:28 +0100

    feat(nvim): change binding psi -> pi

--- nvim/lua/alex/lazy/telescope.lua
+++ nvim/lua/alex/lazy/telescope.lua
@@ -23,7 +23,7 @@ return {
         }
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>pfi', function() builtin.find_files{ no_ignore = true } end, {})
+        vim.keymap.set('n', '<leader>pi', function() builtin.find_files{ no_ignore = true } end, {})
         vim.keymap.set('n', '<C-f>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
